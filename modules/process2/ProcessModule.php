22bd4444f26d6f3ad8788ee51af112ef1322665a
caHek2x <hameleon2x@gmail.com>
2025-08-25 22:48:23 +0300

    мелочи

---
+++ modules/process2/ProcessModule.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace app\modules\process2;
+
+use app\modules\process\assets\ProcessAsset;
+use Yii;
+use yii\base\Module;
+use yii\web\Application as WebApplication;
+
+class ProcessModule extends Module
+{
+    public function init()
+    {
+        parent::init();
+        Yii::configure($this, require(__DIR__ . '/config/web.php'));
+
+        if (Yii::$app instanceof WebApplication) {
+            ProcessAsset::register(Yii::$app->view);
+        }
+    }
+}
---
+++ modules/process2/config/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use yii\helpers\ArrayHelper;
+
+$config = [
+    'params' => [
+
+    ]
+];
+
+if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'web.local.php')) {
+    $local_config = require(__DIR__ . DIRECTORY_SEPARATOR . 'web.local.php');
+    $config = ArrayHelper::merge($config, $local_config);
+}
+return $config;
